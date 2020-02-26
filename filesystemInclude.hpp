19359ab718e3a091850e7591185d9e9c259d6a95
Eugene Yakshin <e.yakshin@wsmgroup.ru>
2020-02-26 10:37:50 +0200

    короткий include и определение неймспейса

---
+++ filesystemInclude.hpp
@@ -0,0 +1,13 @@
+//
+//  filesystem.cpp
+//  UnityMaePlugin
+//
+//  Created by Eugene Yakshin on 2/26/20.
+//  Copyright © 2020 WSM Group. All rights reserved.
+//
+
+#pragma once
+
+#include "include/ghc/filesystem.hpp"
+
+namespace fs = ghc::filesystem;
